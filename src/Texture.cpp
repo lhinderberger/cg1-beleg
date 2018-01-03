e8bea40ece23c27796c1e73f32af5af470fe5039
Lucas Hinderberger <git@lhinderberger.de>
2018-01-03 12:16:34 +0100

    Fixed Texture loading

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -1,6 +1,7 @@
 #include <GL/glew.h>
 #include "Texture.h"
 
+#include <iostream>
 #include <stdexcept>
 #include <FreeImage.h>
 
@@ -9,35 +10,31 @@ using namespace std;
 
 Texture::Texture(const char * filename) {
     /* Load image */
+    cout << "Loading " << filename << endl;
     FREE_IMAGE_FORMAT format = FreeImage_GetFIFFromFilename(filename);
     FIBITMAP * bitmap = FreeImage_Load(format, filename, 0);
     if (!bitmap)
         throw runtime_error("Image loading failed.");
+    FIBITMAP * cBitmap = FreeImage_ConvertTo32Bits(bitmap);
+    FreeImage_Unload(bitmap);
+    if (!cBitmap)
+        throw runtime_error("Image conversion failed.");
         
-    /* Get metadata */
-    int bpp = FreeImage_GetBPP(bitmap);
-    if (bpp != 8)
-        throw runtime_error("Wrong image format (bpp must be 8)");
+    /* Get metadata */    
+    int width = FreeImage_GetWidth(cBitmap);
+    int height = FreeImage_GetHeight(cBitmap);
         
-    FREE_IMAGE_COLOR_TYPE colorType = FreeImage_GetColorType(bitmap);
-    if (colorType != FIC_RGB && colorType != FIC_RGBALPHA)
-        throw runtime_error("Wrong image format (must be RGB / RGBA)");
-    GLuint colorTypeGL = colorType == FIC_RGB ? GL_RGB : GL_RGBA;
-    
-    int width = FreeImage_GetWidth(bitmap);
-    int height = FreeImage_GetHeight(bitmap);
-        
-    unsigned char * imageBytes = FreeImage_GetBits(bitmap);
+    unsigned char * imageBytes = FreeImage_GetBits(cBitmap);
     
     /* Pass on to OpenGL */
     glGenTextures(1, &id);
     glBindTexture(GL_TEXTURE_2D, id);
     
-    glTexImage2D(GL_TEXTURE_2D, 0, colorTypeGL, width, height, 0, colorTypeGL, GL_UNSIGNED_BYTE, imageBytes);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_BGRA, GL_UNSIGNED_BYTE, imageBytes);
     glGenerateMipmap(GL_TEXTURE_2D);
     
     /* Cleanup */
-    FreeImage_Unload(bitmap);
+    FreeImage_Unload(cBitmap);
 }
 
 void Texture::bind() {
